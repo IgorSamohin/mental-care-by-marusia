7c3349c4237cb86a180486d6ca85af64e4d3bf5a
IgorSamohin <igor04012001@yandex.ru>
2022-04-02 19:54:55 +0300

    Add ability to debug on local machine (http://localhost:8080/webhook)

--- src/main/java/com/polis/api/MarusiaController.java
+++ src/main/java/com/polis/api/MarusiaController.java
@@ -6,11 +6,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@CrossOrigin(value = "https://skill-debugger.marusia.mail.ru")
 public class MarusiaController {
     @Autowired
     private MarusiaService service;
