9a5269bb908b0cc99fc6f3c0c990095323146114
IlyaAAAA <ilya-sachuk@mail.ru>
2022-04-03 15:53:53 +0300

    added command enum

--- src/main/java/com/polis/api/storage/Data.java
+++ src/main/java/com/polis/api/storage/Data.java
@@ -1,13 +1,11 @@
 package com.polis.api.storage;
 
+import com.polis.api.Command;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Data {
-
-    private static final String[] EXIT_COMMANDS = new String[]{"пока", "выйди", "хочу выйти", "стоп", "все", "хватит"};
-    private static final String[] HOME_COMMANDS = new String[]{"начало", "вернись в начало", "заново", "давай по новой"};
-
     static final Map<Integer, State> states = new HashMap<>();
 
     static {
@@ -19,101 +17,101 @@ public class Data {
         );
         states.put(
                 -1, new State(-1, MarusiaAnswer.START_STATE_ANSWER.text, MarusiaAnswer.START_STATE_ANSWER.tts, new Transition[]{
-                        new Transition(-3, EXIT_COMMANDS),
-                        new Transition(0, new String[]{"хочу отвлечься", "Давай отдохнём", "Отдых", "Релакс", "отвлечение", "отвлеки"}),
-                        new Transition(1, new String[]{"дай совет", "совет"}),
-                        new Transition(2, new String[]{"Что ты можешь", "Что ты умеешь", "Твои функции", "помощь", "помоги"}),
-                        new Transition(10, new String[]{"как ты можешь меня отвлечь?"}),
-                        new Transition(12, new String[]{"как ты можешь меня успокоить?"}),
-                        new Transition(13, new String[]{"хочу успокоиться", "успокой меня", "успокоение", "успокой"}),
+                        new Transition(-3, Command.EXIT.commands),
+                        new Transition(0, Command.DISTRACTION.commands),
+                        new Transition(1, Command.ADVICE.commands),
+                        new Transition(2, Command.HELP.commands),
+                        new Transition(10, Command.HELP_DISTRACTION.commands),//TODO кажется тут не дб и снизу
+                        new Transition(12, Command.HELP_SEDATION.commands),//TODO
+                        new Transition(13, Command.SEDATION.commands),
                 })
         );
 
         states.put(
                 0, new State(0, MarusiaAnswer.DISTRACTION_STATE_ANSWER.text, MarusiaAnswer.DISTRACTION_STATE_ANSWER.tts, new Transition[]{
-                        new Transition(-3, EXIT_COMMANDS),
-                        new Transition(-1, HOME_COMMANDS),
-                        new Transition(7, new String[]{"Дай йогу", "йога", "Йогу", "Выбираю йогу"}),
-                        new Transition(8, new String[]{"Гадание", "Гадание на числах"}),
-                        new Transition(9, new String[]{"задание на числа", "давай поиграем", "числа", "играть"})
+                        new Transition(-3, Command.EXIT.commands),
+                        new Transition(-1, Command.HOME.commands),
+                        new Transition(7, Command.DISTRACTION_YOGA.commands),
+                        new Transition(8, Command.DISTRACTION_NUMBER_DIVINATION.commands),
+                        new Transition(9, Command.DISTRACTION_COUNT.commands)
                 })
         );
 
         states.put(
                 1, new State(1, MarusiaAnswer.ADVICE_FROM_PSYCHOLOGIST_ANSWER.text, MarusiaAnswer.ADVICE_FROM_PSYCHOLOGIST_ANSWER.tts, new Transition[]{
-                        new Transition(-3, EXIT_COMMANDS),
-                        new Transition(-1, HOME_COMMANDS),
+                        new Transition(-3, Command.EXIT.commands),
+                        new Transition(-1, Command.HOME.commands),
                 })
         );
 
         states.put(
                 2, new State(2, MarusiaAnswer.HELP_ANSWER.text, MarusiaAnswer.HELP_ANSWER.tts, new Transition[]{
-                        new Transition(-3, EXIT_COMMANDS),
-                        new Transition(-1, HOME_COMMANDS),
-                        new Transition(10, new String[]{"как ты можешь отвлечь", "отвлечение"}),
-                        new Transition(12, new String[]{"как ты можешь успокоить", "успокоение"})
+                        new Transition(-3, Command.EXIT.commands),
+                        new Transition(-1, Command.HOME.commands),
+                        new Transition(10, Command.HELP_DISTRACTION.commands),
+                        new Transition(12, Command.HELP_SEDATION.commands)
                 })
         );
 
         states.put(
                 3, new State(3, MarusiaAnswer.VIDEO_ANSWER.text, MarusiaAnswer.VIDEO_ANSWER.tts, new Transition[]{
-                        new Transition(-3, EXIT_COMMANDS),
-                        new Transition(-1, HOME_COMMANDS)
+                        new Transition(-3, Command.EXIT.commands),
+                        new Transition(-1, Command.HOME.commands)
                 })
         );
 
         states.put(
                 4, new State(4, MarusiaAnswer.SOOTHING_SOUND_ANSWER.text, MarusiaAnswer.SOOTHING_SOUND_ANSWER.tts, new Transition[]{
-                        new Transition(-3, EXIT_COMMANDS),
-                        new Transition(-1, HOME_COMMANDS)
+                        new Transition(-3, Command.EXIT.commands),
+                        new Transition(-1, Command.HOME.commands)
                 })
         );
 
         states.put(
                 5, new State(5, MarusiaAnswer.MUSIC_ANSWER.text, MarusiaAnswer.MUSIC_ANSWER.tts, new Transition[]{
-                        new Transition(-3, EXIT_COMMANDS),
-                        new Transition(-1, HOME_COMMANDS)
+                        new Transition(-3, Command.EXIT.commands),
+                        new Transition(-1, Command.HOME.commands)
                 })
         );
 
         states.put(
                 6, new State(6, MarusiaAnswer.BREATHING_EXERCISE_ANSWER.text, MarusiaAnswer.BREATHING_EXERCISE_ANSWER.tts, new Transition[]{
-                        new Transition(-3, EXIT_COMMANDS),
-                        new Transition(-1, HOME_COMMANDS)
+                        new Transition(-3, Command.EXIT.commands),
+                        new Transition(-1, Command.HOME.commands)
                 })
         );
 
         states.put(
                 7, new State(7, MarusiaAnswer.YOGA_ANSWER.text, MarusiaAnswer.YOGA_ANSWER.tts, new Transition[]{
-                        new Transition(-3, EXIT_COMMANDS),
-                        new Transition(-1, HOME_COMMANDS)
+                        new Transition(-3, Command.EXIT.commands),
+                        new Transition(-1, Command.HOME.commands)
                 })
         );
 
         states.put(
                 9, new State(9, MarusiaAnswer.NUMBER_COUNT_TASK_ANSWER.text, MarusiaAnswer.NUMBER_COUNT_TASK_ANSWER.tts, new Transition[]{
-                        new Transition(-3, EXIT_COMMANDS),
-                        new Transition(-1, HOME_COMMANDS)
+                        new Transition(-3, Command.EXIT.commands),
+                        new Transition(-1, Command.HOME.commands)
                 })
         );
-        states.put(10, new State(10, MarusiaAnswer.DISTRACTION_HELP_ANSWER.text, MarusiaAnswer.DISTRACTION_HELP_ANSWER.tts, new Transition[]{
-                        new Transition(-3, EXIT_COMMANDS),
-                        new Transition(-1, HOME_COMMANDS)
+        states.put(10, new State(10, MarusiaAnswer.HELP_DISTRACTION_ANSWER.text, MarusiaAnswer.HELP_DISTRACTION_ANSWER.tts, new Transition[]{
+                        new Transition(-3, Command.EXIT.commands),
+                        new Transition(-1, Command.HOME.commands)
                 })
         );
-        states.put(12, new State(12, MarusiaAnswer.SEDATION_HELP_ANSWER.text, MarusiaAnswer.SEDATION_HELP_ANSWER.tts, new Transition[]{
-                        new Transition(-3, EXIT_COMMANDS),
-                        new Transition(-1, HOME_COMMANDS)
+        states.put(12, new State(12, MarusiaAnswer.HELP_SEDATION_ANSWER.text, MarusiaAnswer.HELP_SEDATION_ANSWER.tts, new Transition[]{
+                        new Transition(-3, Command.EXIT.commands),
+                        new Transition(-1, Command.HOME.commands)
                 })
         );
 
         states.put(13, new State(13, MarusiaAnswer.SEDATION_STATE_ANSWER.text, MarusiaAnswer.SEDATION_STATE_ANSWER.tts, new Transition[]{
-                        new Transition(-3, EXIT_COMMANDS),
-                        new Transition(-1, HOME_COMMANDS),
-                        new Transition(3, new String[]{"Скинь видос", "Скинь видео", "Видео", "видос"}),
-                        new Transition(4, new String[]{"Включи успокаивающие звуки", "успокаивающие звуки", "спокойные звуки", "звуки"}),
-                        new Transition(5, new String[]{"Включи успокаивающую музыку", "музыка"}),
-                        new Transition(6, new String[]{"Давай подышим", "Давай пыхнем", "Дыхательная гимнастика", "Дышать", "Вздох"})
+                        new Transition(-3, Command.EXIT.commands),
+                        new Transition(-1, Command.HOME.commands),
+                        new Transition(3, Command.SEDATION_VIDEO.commands),
+                        new Transition(4, Command.SEDATION_SOUNDS.commands),
+                        new Transition(5, Command.SEDATION_MUSIC.commands),
+                        new Transition(6, Command.BREATH_EXERCISE.commands)
                 })
         );
     }
