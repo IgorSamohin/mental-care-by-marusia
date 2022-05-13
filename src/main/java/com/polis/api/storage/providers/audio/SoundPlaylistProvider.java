44f9ee8fc8d07682eaa118cef45139788f18d09c
IlyaAAAA <ilya-sachuk@mail.ru>
2022-05-13 16:48:33 +0300

    added sound without ap

--- src/main/java/com/polis/api/storage/providers/audio/SoundPlaylistProvider.java
+++ src/main/java/com/polis/api/storage/providers/audio/SoundPlaylistProvider.java
@@ -21,5 +21,24 @@ public class SoundPlaylistProvider extends PlayListProvider {
         playLists.add(new PlayList("2000512010_456239093"));
         playLists.add(new PlayList("2000512010_456239094"));
         playLists.add(new PlayList("2000512010_456239096"));
+        playLists.add(new PlayList("2000512010_456239098"));
+        playLists.add(new PlayList("2000512010_456239099"));
+        playLists.add(new PlayList("2000512010_456239100"));
+        playLists.add(new PlayList("2000512010_456239102"));
+        playLists.add(new PlayList("2000512010_456239103"));
+        playLists.add(new PlayList("2000512010_456239104"));
+        playLists.add(new PlayList("2000512010_456239105"));
+        playLists.add(new PlayList("2000512010_456239106"));
+        playLists.add(new PlayList("2000512010_456239107"));
+        playLists.add(new PlayList("2000512010_456239108"));
+        playLists.add(new PlayList("2000512010_456239109"));
+        playLists.add(new PlayList("2000512010_456239110"));
+        playLists.add(new PlayList("2000512010_456239111"));
+        playLists.add(new PlayList("2000512010_456239112"));
+        playLists.add(new PlayList("2000512010_456239113"));
+        playLists.add(new PlayList("2000512010_456239114"));
+        playLists.add(new PlayList("2000512010_456239115"));
+        playLists.add(new PlayList("2000512010_456239116"));
+
     }
 }
